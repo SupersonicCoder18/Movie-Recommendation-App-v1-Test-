dc59a0955c12555536ba2665df24e227ae3354c5
Sravas Melarkode <63852545+SupersonicCoder18@users.noreply.github.com>
2021-01-29 16:50:34 +0530

    Add files via upload
    only testing

---
+++ screens/recommended.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react"
+import { View, StyleSheet, FlatList } from "react-native"
+import { Card } from "react-native-elements"
+import { RFValue } from "react-native-responsive-fontsize"
+import axios from "axios"
+
+export default class RecommendedMoviesScreen extends Component{
+    constructor(){
+        super()
+        this.state = {
+            data: []
+        }
+    }
+    
+    componentDidMount(){
+        this.getData()
+    }
+
+    timeConvert(num){
+        var hours = Math.floor(num/60)
+        var minutes = num%60
+        return `${hours} hr ${minutes} mins`;
+    }
+
+    getData = () => {
+        const url = "http://localhost:5000/recommended-movies"
+        axios
+            .get(url)
+            .then(async response => {
+                this.setState({
+                    data: response.data.data
+                })
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
+
+    keyExtractor = (item, index) => index.toString()
+    renderItems = ({items, index}) => {
+        return (
+            <Card key = {`card-${index}`} image = {{uri: poster_link}} imageProps = {{resizeMode: "cover"}}
+            featureTitle = {item.title}
+            containerStyle = {styles.cardContainer}
+            featureTitleStyle = {styles.title}
+            featureSubTitle = {`${item.release_date.split("-")[0]} | ${tjis.timeConvert(item.duration)}`}
+            featureSubTitleStyle = {styles.subTitle}>
+            </Card>
+        )
+    }
+    render(){
+        const {data} = this.state
+        return (
+            <View style = {styles.container}>
+                <FlatList data = {data}
+                keyExtractor = {this.keyExtractor}
+                renderItem = {this.renderItems}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff"
+    },
+    title: {
+      color: "#fff",
+      alignSelf: "flex-start",
+      paddingLeft: RFValue(15),
+      fontSize: RFValue(25),
+      marginTop: RFValue(65)
+    },
+    subtitle: {
+      fontWeight: "bold",
+      alignSelf: "flex-start",
+      paddingLeft: RFValue(15),
+      fontSize: RFValue(15)
+    },
+    cardContainer: {
+      flex: 1,
+      borderRadius: RFValue(10),
+      justifyContent: "center",
+      height: RFValue(110),
+      marginBottom: RFValue(20)
+    }
+  });
